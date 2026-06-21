9aad2068da0ed942a1a36af46e816f54928b5c92
Olexandr Zhytariuk <olexandrzhytaryuk@gmail.com>
2026-06-21 23:25:28 +0300

    basic object pooling

--- Runtime/FloatingTextFactory.cs
+++ Runtime/FloatingTextFactory.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace FloatingText
 {
@@ -6,15 +8,53 @@ namespace FloatingText
     {
         private GameObject _floatingTextPrefab;
 
+        private Dictionary<int, ObjectPool<FloatingTextView>> _objectPools = new();
+
         public FloatingTextFactory()
         {
             _floatingTextPrefab = Resources.Load<FloatingTextView>("FloatingText Prefab (TMP)").gameObject;
         }
 
-        public FloatingTextView Create()
+        public FloatingTextView Create(int style = 0)
         {
-            var newTextGO = GameObject.Instantiate(_floatingTextPrefab);
-            return newTextGO.GetComponent<FloatingTextView>();
+            var pool = GetPoolByStyle(style);
+            return pool.Get();
+        }
+
+        private ObjectPool<FloatingTextView> GetPoolByStyle(int style)
+        {
+            if (!_objectPools.ContainsKey(style))
+            {
+                _objectPools[style] = InstantiateNewPool();
+            }
+
+            return _objectPools[style];
+        }
+
+        private ObjectPool<FloatingTextView> InstantiateNewPool()
+        {
+            return new ObjectPool<FloatingTextView>
+            (
+                createFunc: () =>
+                {
+                    var view = Object.Instantiate(_floatingTextPrefab);
+                    return view.GetComponent<FloatingTextView>();
+                },
+                actionOnGet: view =>
+                {
+                    view.gameObject.SetActive(true);
+                },
+                actionOnRelease: view =>
+                {
+                    view.gameObject.SetActive(false);
+                },
+                actionOnDestroy: view =>
+                {
+                    Object.Destroy(view.gameObject);
+                },
+                collectionCheck: false,
+                maxSize: 2048
+            );
         }
     }
 }
