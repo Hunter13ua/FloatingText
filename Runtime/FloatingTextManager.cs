f84f66fca15f12e5c6eb3090d9946c02d6b0c20a
Olexandr Zhytariuk <olexandrzhytaryuk@gmail.com>
2026-06-21 22:37:38 +0300

    base performance metrics (0.3ms spawn; 1.2ms/1k update)

--- Runtime/FloatingTextManager.cs
+++ Runtime/FloatingTextManager.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using Unity.Profiling;
 using UnityEngine;
 
 namespace FloatingText
 {
     internal class FloatingTextManager : MonoBehaviour
     {
+        static private readonly ProfilerMarker SpawnMarker = new("FloatingTextManager.Spawn");
+        
         private GameObject _floatingTextPrefab;
 
         private List<FloatingTextView> _activeTexts = new ();
@@ -16,6 +19,8 @@ namespace FloatingText
 
         public void Spawn(Vector3 position, string text)
         {
+            SpawnMarker.Begin();
+
             var newTextGO = Instantiate(_floatingTextPrefab, position, Quaternion.identity);
             var textView = newTextGO.GetComponent<FloatingTextView>();
             textView.SetupComponent(text);
@@ -23,13 +28,15 @@ namespace FloatingText
             _activeTexts.Add(textView);
 
             // TODO: implement pooling, animation, and cleanup
+
+            SpawnMarker.End();
         }
 
         private void Update()
         {
             foreach (var text in _activeTexts)
             {
-                text.transform.position += Vector3.up * Time.deltaTime;
+                text.transform.position += Vector3.up * Time.deltaTime * 0.1f;
             }
         }
     }
